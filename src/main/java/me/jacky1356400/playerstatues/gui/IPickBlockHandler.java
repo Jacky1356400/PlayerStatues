f4a004e2baa45fad421e474135f28d0f9059567e
Jacky1356400 <jacky1356400@hotmail.com>
2017-08-15 01:27:49 +0800

    Alternate port
    Trying to port via the original code directly, rather than from
    scratch...

---
+++ src/main/java/me/jacky1356400/playerstatues/gui/IPickBlockHandler.java
@@ -0,0 +1,7 @@
+package me.jacky1356400.playerstatues.gui;
+
+import net.minecraft.item.ItemStack;
+
+public interface IPickBlockHandler {
+	void blockPicked(ItemStack stack);
+}
---
+++ src/main/java/me/jacky1356400/playerstatues/statues/ContainerStatue.java
@@ -0,0 +1,27 @@
+/**
+ * Container class for the statue
+ */
+
+package me.jacky1356400.playerstatues.statues;
+
+import net.minecraft.entity.player.InventoryPlayer;
+import pl.asie.lib.block.ContainerBase;
+
+public class ContainerStatue extends ContainerBase {
+	TileEntityStatue	tile;
+
+	public ContainerStatue(InventoryPlayer inventory, TileEntityStatue te) {
+		super(te, inventory);
+		tile = te;
+
+		addSlotToContainer(new SlotArmorStatue(tile, 0, 80, 33, 0));
+		addSlotToContainer(new SlotArmorStatue(tile, 1, 80, 51, 1));
+		addSlotToContainer(new SlotArmorStatue(tile, 2, 80, 69, 2));
+		addSlotToContainer(new SlotArmorStatue(tile, 3, 80, 87, 3));
+		addSlotToContainer(new SlotHand(tile, 4, 111, 51));
+		addSlotToContainer(new SlotHand(tile, 5, 49, 51));
+		
+		this.bindPlayerInventory(inventory, 8, 144);
+	}
+
+}
